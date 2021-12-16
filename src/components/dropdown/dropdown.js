f8a009047cf71a1e2c8834f8482543607543c584
rinakulikova185 <irinakulikova0185@gmail.com>
2021-12-16 09:12:20 +0300

    script for guests dropdown refactoring

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -7,57 +7,79 @@ document.querySelectorAll(".dropdown").forEach(dropdownWrapper => {
         dropdownBtn = dropdownWrapper.querySelector(".dropdown__button"),
         dropdownList = dropdownWrapper.querySelector(".dropdown__list"),
         quantityBlocks = dropdownWrapper.querySelectorAll(".dropdown__amount"),
-        dropdownListItem = dropdownWrapper.querySelectorAll(".dropdown__item"),
         applyBtn = dropdownWrapper.querySelector(".dropdown__apply"),
         clearBtn = dropdownWrapper.querySelector(".dropdown__clear");
+ 
+  dropdownBtn.addEventListener('click', () => {
+    dropdownList.classList.toggle("dropdown__list_visible")
+  })
 
-
-
-dropdownBtn.addEventListener('click', () => {
-  dropdownList.classList.toggle("dropdown__list_visible")
-})
-
-applyBtn.addEventListener('click', () => {
-  const valuesArray = Array.from( quantityBlocks, item => +item.innerHTML);
-  const total = valuesArray.reduce((prev,current) => prev + current);
-  let unit
-  if(total === 1) {
-    unit = "гость"
-  } else if ( total >= 2  && total <= 4) {
-    unit = "гостя"
-  } else {
-    unit = "гостей"
+  function getValuesArray() {
+    return Array.from( quantityBlocks, item => +item.innerHTML);
   }
-  dropdownBtn.innerHTML = `${total} ${unit}`
-  
-})
 
-decrementBtns.forEach(btn => btn.addEventListener('click', () => {
-  changeQuantity(btn, -1)
-}))
-
-incrementBtns.forEach(btn => btn.addEventListener('click', () => {
-  changeQuantity(btn, 1)
-}))
-
-function changeQuantity(item, value) {
-  const quantityBlock = item.parentElement.querySelector(".dropdown__amount");
-  const currentQuantity = +quantityBlock.innerHTML;
-  let newQuantity = currentQuantity + value;
-  if(newQuantity >= 0) {
-    quantityBlock.innerHTML = newQuantity;
+  function handleApplyBtnClick() {
+    const total = getValuesArray().reduce((prev,current) => prev + current);
+    let unit
+    if(total === 1) {
+      unit = "гость"
+    } else if ( total >= 2  && total <= 4) {
+      unit = "гостя"
+    } else {
+      unit = "гостей"
+    }
+    dropdownBtn.innerHTML = `${total} ${unit}`
   }
-}
-
-dropdownList.addEventListener('click', (e) => {
-  e.stopPropagation()
-})
-
-document.addEventListener('click', (e) => {
-  console.log(e.target)
-  if(e.target !== dropdownBtn) {
-    dropdownList.classList.remove("dropdown__list_visible")
+  applyBtn.addEventListener('click', () => {
+    handleApplyBtnClick();
+  })
+  function isClearBtnHidden(value) {
+    return value>0 && !clearBtn.classList.contains("dropdown__clear_visible")
   }
-})
+  function shouldClearBtnHidden(value) {
+    return value === 0  && getValuesArray().every(num => num === 0)
+  }
+  function handleControlBtnClick(item, num) {
+    const quantityBlock = item.parentElement.querySelector(".dropdown__amount");
+    const currentValue = +quantityBlock.innerHTML;
+    let newValue = currentValue + num;
+    if(newValue >= 0) {
+      quantityBlock.innerHTML = newValue;
+    }
+    if(isClearBtnHidden(newValue)) {
+      clearBtn.classList.add("dropdown__clear_visible")
+    }
+    if(shouldClearBtnHidden(newValue)) {
+      clearBtn.classList.remove("dropdown__clear_visible")
+    }
+  }
+
+  decrementBtns.forEach(btn => btn.addEventListener('click', () => {
+    handleControlBtnClick(btn, -1)
+  }))
+
+  incrementBtns.forEach(btn => btn.addEventListener('click', () => {
+    handleControlBtnClick(btn, 1)
+  }))
+
+  function handleClearBtnClick() {
+    quantityBlocks.forEach(block => block.innerHTML = 0)
+    clearBtn.classList.remove("dropdown__clear_visible")
+  }
+
+  clearBtn.addEventListener('click', () => {
+    handleClearBtnClick()
+  })
+
+  dropdownList.addEventListener('click', (e) => {
+    e.stopPropagation()
+  })
+
+  document.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target !== dropdownBtn) {
+      dropdownList.classList.remove("dropdown__list_visible")
+    }
+  })
 })
 
